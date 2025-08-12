8a9afc252c2859ae96693d4284fe3e3956bce18e
SAKTHIMAHENTHAR_S <sakthimahenthar23@gmail.com>
2025-08-12 12:09:41 +0530

    Create 647_Palindromic_Substrings.java

---
+++ 647_Palindromic_Substrings.java
@@ -0,0 +1,68 @@
+/*647. Palindromic Substrings
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+                          program   */
+
+
+
+
+class Solution {
+    public int countSubstrings(String s) {
+    
+    int count = 0;
+    for(int i=0;i<s.length();i++){
+    StringBuilder sb = new StringBuilder();
+        for(int j=i;j<s.length();j++){
+            sb.append(s.charAt(j));
+            // System.out.print(sb+"- ");
+            if(ispal(sb)){
+                count++;
+            }
+        }
+    }
+    return count;
+    }
+    static boolean ispal(StringBuilder s){
+        if(s.length()<2){
+            return true;
+        }
+       int left =0,right = s.length()-1;
+       while(left<right){
+        if(s.charAt(left) != s.charAt(right)){
+            return false;
+        }
+        left++;
+        right--;
+       }
+       return true;
+    }
+
+}
